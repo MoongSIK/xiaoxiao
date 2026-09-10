15cac0e1ae359f05a93a9ac6881c905dc438546a
MoongSIK <maria1241@naver.com>
2026-09-10 13:27:57 +0000

    Set up GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/xiaoxiao",
+  assetPrefix: "/xiaoxiao/",
 };
 
 export default nextConfig;
